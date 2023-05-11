33203f9c628a50b69d7dfb4ac65b3c3012a4c9a8
Yao Jing Quek <yao.jing.quek@intel.com>
2023-05-11 09:08:28 +0800

    Rohd website (#7)
    * Initial commit
    
    * Fix markdown lint in overview
    
    * Fix installation markdown linting
    
    * Fixed A01 markdown lint
    
    * Fix A02
    
    * Fix the linting issue
    
    * Added rohd api submodule
    
    * Ignore the lint check
    
    * update the name from rohd to docs
    
    * update base site name
    
    * Ignore api link
    
    * Fix setup broken link
    
    * update link to dynamic
    
    * Remove git submodule
    
    * add git submodule
    
    * change git module to git subtree
    
    * Squashed 'rohd/api-rohd/' content from commit df40d8ea
    
    git-subtree-dir: rohd/api-rohd
    git-subtree-split: df40d8ea7aeef7c1dda405c438d5489d487b9cc3
    
    * update the api path
    
    * Squashed 'docs/api-rohd/' content from commit df40d8ea
    
    git-subtree-dir: docs/api-rohd
    git-subtree-split: df40d8ea7aeef7c1dda405c438d5489d487b9cc3
    
    * removed all api
    
    * Squashed 'docs/api/' content from commit df40d8ea
    
    git-subtree-dir: docs/api
    git-subtree-split: df40d8ea7aeef7c1dda405c438d5489d487b9cc3
    
    * Fix all broken link - PLease work
    
    * update the structure of the code
    
    * update the github actions
    
    * Temporary update the branch name
    
    * update the identation
    
    * Fix missing steps
    
    * update missing property
    
    * Add repo checkout
    
    * change the push and pull branches to main
    
    * update the file name
    
    * update the workflow to test
    
    * update on-push to the update_website yml
    
    * update the merge pull request
    
    * delete merge pull request
    
    * Remove approved
    
    * Add workflow dispatch
    
    * Add test code
    
    * Try to fix the update website schedule
    
    * temporary add push to the branches
    
    * destination branch is wrong
    
    * update branch name
    
    * delete branch false to check issue
    
    * update source content
    
    * fix missing folder
    
    * add debug messages
    
    * debug msg
    
    * Add branch creation
    
    * user
    
    * update sequence
    
    * Check git status
    
    * Create staging branch
    
    * update staging
    
    * Show git status
    
    * Do not ignore as we will need for website construction
    
    * update workflow
    
    * update gitignore
    
    * Remove useless content
    
    * update deployment branch
    
    * delete branch set to true
    
    * change the direction of push
    
    * change branch name to deploy website
    
    * Do not delete branch yet
    
    * Need to commit to staging branch
    
    * update the website
    
    * update the API stuff and naming convention
    
    * update folder name
    
    * setup dart lang and documentation structure
    
    * update directory name
    
    * Clean the CI files
    
    * Removed git diff
    
    * Delete zone identifier
    
    * Delete zone identifier
    
    * update the submodule
    
    * Points rohd to correct direction

---
+++ tool/gh_actions/update_directory.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (C) 2023 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# update_directory.sh
+# GitHub Actions step: Set up the files directory
+#
+# 2023 May 3
+# Author: Yao Jing Quek <yao.jing.quek@intel.com>
+
+set -euo pipefail
+
+root_directory=$(git rev-parse --show-toplevel)
+
+src_path="$root_directory/docs"
+rohd_submodule_path="$root_directory/rohd"
+
+cd rohd
+dart doc -o doc/user_guide/api
+cd ..
+
+for directory in "$rohd_submodule_path/doc/user_guide"/*; do
+    directory_name=$(basename "$directory")
+
+    if [ -d "$src_path/$directory_name" ]; then
+        echo "directory exist, removing the old directory before copy."
+        rm -r "$src_path/$directory_name"
+    else
+        echo "directory not exist, proceed to copy."
+    fi
+
+    cp -r "$directory" "$src_path/$directory_name"
+done
+
+
